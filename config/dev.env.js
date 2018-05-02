334bf22f6affb42bdbe05478010cd30a21f72272
Jachin <874545946@qq.com>
2018-05-02 20:35:17 +0800

    Yardman_system
    Api配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // API_ROOT: '"http://lostars.cn/answer.php"'
+  API_ROOT: '"http://lostars.cn/answer.php"',
+  API_ROOT2: '"http://lostars.cn/students.php"',
 })
